51bdd285be1fd8916b86361424fbfdccb19154e7
Samarth <163443439+samarthgalhe89@users.noreply.github.com>
2026-02-04 15:22:34 +0530

    feat: landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumX" },
+    { name: "description", content: "ResumX: Because Recruiters Scan… We Analyze." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track your Applications & Resume Ratings!</h1>
+        <h2>Upload your resume and let AI handle the rest.</h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume: Resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+        </div>
+      )}
+    </section>
+
+
+  </main>
 }
